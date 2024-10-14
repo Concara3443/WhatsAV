87186b4b7db2d78b3cf8e61da7005aa92e4cb0a6
Guillermo Cortés <Cortesmesas@gmail.com>
2024-10-15 01:45:12 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const fs = require('fs');
+
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    },
+    authStrategy: new LocalAuth({
+        dataPath: 'session'
+    })
+});
+
+// Definir algunas colecciones globales
+client.commands = new Map();
+client.cooldowns = new Map();
+client.aliases = new Map();
+client.categories = fs.readdirSync('./src/commands/');
+
+const handlers = ['events', 'commands', 'antiCrash'];
+
+handlers.forEach(h => {
+    const handler = require(`./handlers/${h}`);
+    if (typeof handler === 'function') {
+      handler(client);
+    } else {
+      console.error(`Handler ${h} is not a function`);
+    }
+  });
+
+client.initialize();
