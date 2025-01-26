9e786fa3df1ba87d24472a028b2e7d638137a765
Guillermo Cortés <Cortesmesas@gmail.com>
2025-01-26 18:50:33 +0100

    Refactor index.js for consistent code style and formatting

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const fs = require('fs');
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const fs = require("fs");
 
 const client = new Client({
-    puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    },
-    authStrategy: new LocalAuth({
-        dataPath: 'session'
-    })
+  puppeteer: {
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+  authStrategy: new LocalAuth(),
 });
 
 // Definir algunas colecciones globales
 client.commands = new Map();
 client.cooldowns = new Map();
 client.aliases = new Map();
-client.categories = fs.readdirSync('./src/commands/');
+client.categories = fs.readdirSync("./src/commands/");
 
 // const handlers = ['events', 'commands', 'antiCrash'];
-const handlers = ['events', 'commands'];
+const handlers = ["events", "commands"];
 
-handlers.forEach(h => {
-    const handler = require(`./handlers/${h}`);
-    if (typeof handler === 'function') {
-      handler(client);
-    } else {
-      console.error(`Handler ${h} is not a function`);
-    }
-  });
+handlers.forEach((h) => {
+  const handler = require(`./handlers/${h}`);
+  if (typeof handler === "function") {
+    handler(client);
+  } else {
+    console.error(`Handler ${h} is not a function`);
+  }
+});
 
 client.initialize();
