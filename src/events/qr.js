1c364eb9a92b40c02d96a7cd923fdb9150e5a376
Guillermo Cortés <Cortesmesas@gmail.com>
2025-01-26 18:46:30 +0100

    Remove debug log from QR code generation event handler

--- src/events/qr.js
+++ src/events/qr.js
@@ -2,7 +2,6 @@ const qrcode = require('qrcode-terminal');
 
 module.exports = (client, qr) => {
   try{
-      console.log("Soy yo", qr)
       qrcode.generate(qr, {small: true});
     } catch (e){
       console.log(String(e.stack))
